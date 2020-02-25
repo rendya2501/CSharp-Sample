4d4f0ccbb80eca342660569815a9a2ff37ed7b2e
rendya2501 <rendya2501@gmail.com>
2020-02-26 00:30:00 +0900

    async1

--- AsyncAwait1/Program.cs
+++ AsyncAwait1/Program.cs
@@ -7,22 +7,27 @@ namespace AsyncAwait1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Task T = RunAsync();
+            // ①非同期メソッドを呼び出す
+            Task T = RunAsync(); //③処理が戻る
             Console.WriteLine("...他の処理...");
+            T.Wait();
         }
 
+        /// <summary>
+        /// 非同期メソッドの定義
+        /// </summary>
+        /// <returns></returns>
         private static async Task RunAsync()
         {
-            await Task.Run(() => Count(1));
-            Console.WriteLine("処理が終了しました。");
+            await Task.Run(() => Count(1)); // ②呼び出し元に処理を戻す
+            Console.WriteLine("処理が終了しました。"); // Countメソッドが終了してから実行される
         }
 
         static void Count(int n)
         {
             for (int i = 0; i < 50; i++)
             {
-                Console.WriteLine($"Task{n}] {i}");
+                Console.WriteLine($"Task{n}: {i}");
             }
         }
     }
