472680fdf347ad7b1e4c31a7e373d31e6cceb245
rendya2501 <rendya2501@gmail.com>
2020-02-26 22:54:25 +0900

    AsyncAwait3

--- AsyncAwait3/Program.cs
+++ AsyncAwait3/Program.cs
@@ -12,11 +12,35 @@ namespace AsyncAwait3
         /// <returns></returns>
         static async Task Main()
         {
-            var client = new WebClient();
-            var result = await client.DownloadStringTaskAsync("https://codezine.jp/");
-            Console.WriteLine("1");
+            //var client = new WebClient();
+            //var result = await client.DownloadStringTaskAsync("https://codezine.jp/");
+            //Console.WriteLine("1");
+            //Console.WriteLine(result);
+            //Console.WriteLine("2");
+            var result = await New();
             Console.WriteLine(result);
-            Console.WriteLine("2");
+        }
+
+        private static async Task<string> New()
+        {
+            var client = new WebClient();
+            return await client.DownloadStringTaskAsync("https://codezine.jp/");
+        }
+
+        /// <summary>
+        /// 従来の非同期通信
+        /// </summary>
+        private static void Old()
+        {
+            var client = new WebClient();
+            // 非同期処理完了時の処理をイベントに登録
+            client.DownloadStringCompleted += (sender, e) =>
+            {
+                Console.WriteLine(e.Result);
+            };
+
+            client.DownloadStringAsync(new Uri("https://codezine.jp"));
+            Console.ReadLine();
         }
     }
 }
