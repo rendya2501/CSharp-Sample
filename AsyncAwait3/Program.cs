b3b0709c9fcb3c60c2a5b19dcc2d2ee10e387c3b
s.ito <s.ito@alp-inc.jp>
2020-02-26 18:15:21 +0900

    AsyncAwait3

---
+++ AsyncAwait3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace AsyncAwait3
+{
+    class Program
+    {
+        /// <summary>
+        /// 非同期メソッドの利用例
+        /// </summary>
+        /// <returns></returns>
+        static async Task Main()
+        {
+            var client = new WebClient();
+            var result = await client.DownloadStringTaskAsync("https://codezine.jp/");
+            Console.WriteLine("1");
+            Console.WriteLine(result);
+            Console.WriteLine("2");
+        }
+    }
+}
