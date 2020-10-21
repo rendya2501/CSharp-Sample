208c7270d79591c7571887066b223a76004e9297
s.ito <s.ito@alp-inc.jp>
2020-10-21 19:18:07 +0900

    wpfサンプル微修正

--- BlankCoreApp1/ViewModels/MainWindowViewModel.cs
+++ BlankCoreApp1/ViewModels/MainWindowViewModel.cs
@@ -15,25 +15,17 @@ namespace BlankCoreApp1.ViewModels
         private string _title = "Prism Application";
         public string Title
         {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
+            get => _title;
+            set => SetProperty(ref _title, value);
         }
 
         private bool isNonMark = false;
         public bool IsNonMark
         {
-            get { return isNonMark; }
+            get => isNonMark;
             set
             {
                 SetProperty(ref isNonMark, value);
-                //if (isNonMark)
-                //{
-                //    factory = new NonMarkLetterFactory();
-                //}
-                //else
-                //{
-                //    factory = new AllLetterFactory();
-                //}
                 factory = isNonMark
                     ? (ILetterFactory)new NonMarkLetterFactory()
                     : new AllLetterFactory();
@@ -42,15 +34,15 @@ namespace BlankCoreApp1.ViewModels
         private int numOfLetters;
         public int NumOfLetters
         {
-            get { return numOfLetters; }
-            set { SetProperty(ref numOfLetters, value); }
+            get => numOfLetters;
+            set => SetProperty(ref numOfLetters, value);
         }
 
         private string createdPassword;
         public string CreatedPassword
         {
-            get { return createdPassword; }
-            set { SetProperty(ref createdPassword, value); }
+            get => createdPassword;
+            set => SetProperty(ref createdPassword, value);
         }
 
         public DelegateCommand Generate { get; private set; }
@@ -66,7 +58,7 @@ namespace BlankCoreApp1.ViewModels
             IsNonMark = false;
             CreatedPassword = "This is Prism WPF App.";
             Generate = new DelegateCommand(
-                () => { CreatePasswordExecute(); },
+                () => CreatePasswordExecute(),
                 () => CanMakePasswordExecute()
             );
         }
@@ -80,10 +72,7 @@ namespace BlankCoreApp1.ViewModels
             CreatedPassword = generator.MakePassword(NumOfLetters, factory);
         }
 
-        public bool CanMakePasswordExecute()
-        {
-            return numOfLetters > 10;
-        }
+        public bool CanMakePasswordExecute() => numOfLetters > 10;
         #endregion
     }
 }
