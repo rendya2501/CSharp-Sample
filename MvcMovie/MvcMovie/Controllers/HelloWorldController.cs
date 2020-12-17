de35bc60fb9dfb5fe73b3a1b6d0ba8ed76bcc892
rendya2501 <rendya2501@gmail.com>
2020-12-18 00:49:20 +0900

    other sil

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: \HelloWorld
+        public IActionResult Index()
+        {
+            //return "This is my default action...";
+            return View();
+        }
+
+        //GET: \HelloWorld\Welcome\
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name}, ID : {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
