1f8968ad25df36a4230cb0f09731154bcb65d8ef
s.ito <s.ito@alp-inc.jp>
2021-01-10 17:56:13 +0900

    Nullable検証のため

---
+++ CSharpSample1/NullableDictionary2/DictionaryConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NullableDictionary2
+{
+    /// <summary>
+    /// DictionaryのKeyをValueに変換するコンバーター
+    /// </summary>
+    public class DictionaryConverter : IValueConverter
+    {
+        /// <summary>
+        /// DictionaryのKeyをValueに変換します。
+        /// </summary>
+        /// <param name="value">バインディング ソースによって生成された値</param>
+        /// <param name="targetType">バインディング ターゲット プロパティの型</param>
+        /// <param name="parameter">使用するコンバーター パラメーター</param>
+        /// <param name="culture">コンバーターで使用するカルチャ</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // 値、型チェック
+            if (value == null) return null;
+            if (!(parameter is IDictionary)) throw new Exception(string.Format("型"));
+            // パラメータの型変換
+            var dictionary = (IDictionary)parameter;
+            // インデクサーで値を取得
+            return dictionary[value];
+        }
+        /// <summary>
+        /// OneWayでのBindingでしか使用しません。
+        /// </summary>
+        /// <param name="value">バインディング ソースによって生成された値</param>
+        /// <param name="targetType">バインディング ターゲット プロパティの型</param>
+        /// <param name="parameter">使用するコンバーター パラメーター</param>
+        /// <param name="culture">コンバーターで使用するカルチャ</param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ CSharpSample1/NullableDictionary2/ValidFlagItemSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullableDictionary2
+{
+    /// <summary>
+    /// 使用フラグアイテムソース
+    /// </summary>
+    public static class ValidFlagItemSource
+    {
+        /// <summary>
+        /// すべてを含む一覧
+        /// </summary>
+        public static IList<KeyValuePair<bool?, string>> ValidFlagListHasBlank
+        {
+            get
+            {
+                return new List<KeyValuePair<bool?, string>>()
+                {
+                    new KeyValuePair<bool?, string>(null, "すべて"),
+                    new KeyValuePair<bool?, string>(true, "使用する"),
+                    new KeyValuePair<bool?, string>(false, "使用しない"),
+                };
+            }
+        }
+    }
+
+}
