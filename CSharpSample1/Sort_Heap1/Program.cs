a91d40164a7933f6e6248abcd4ddb7af0dfdd1a8
rendya2501 <rendya2501@gmail.com>
2021-09-15 23:38:19 +0900

    9月15日分

---
+++ CSharpSample1/Sort_Heap1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sort_Heap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JavaHeapSort.Execute();
+        }
+    }
+}
--- CSharpSample1/Sort_Merge1/CMergeSort.cs
+++ CSharpSample1/Sort_Merge1/CMergeSort.cs
@@ -10,6 +10,7 @@ namespace Sort_Merge1
         /// </summary>
         public static void Execute()
         {
+            Console.WriteLine("C言語版");
             var array = new int[10] { 5, 0, 9, 7, 1, 6, 3, 8, 4, 2 };
             var work = new int[10];
             Console.WriteLine(string.Join(",", array));
@@ -27,15 +28,39 @@ namespace Sort_Merge1
         private static void MergeSort(int[] array, int left, int right, int[] work)
         {
             // ソートを行うデータ数が1つになった場合は処理終了
-            if (left == right) return;
+            if (left == right)
+            {
+                return;
+            }
 
             // 集合を中央で2つに分割する
             // left = midの集合とmid+1 ~ rightの集合に分割
             var mid = (left + right) / 2;
 
             // 分割後の各集合のデータをそれぞれソートする
+            // 左側 left ~ mid
             MergeSort(array, left, mid, work);
+            // 右側 mid+1 ~ right
             MergeSort(array, mid + 1, right, work);
+
+            // 全体の流れ
+            // (0,4),(5,9)
+
+            // (0,4)
+            //   |---------------¬
+            // (0,2)           (3,4)
+            //   |---------¬     |-----¬
+            // (0,1)     (2,2) (3,3) (4,4)
+            //   |-----¬
+            // (0,0) (1,1)
+
+            // (5,9)
+            //   |---------------¬
+            // (5,7)           (8,9)
+            //   |---------¬     |-----¬
+            // (5,6)     (7,7) (8,8) (9,9)
+            //   |-----¬
+            // (5,5) (6,6)
 
             // ソート済みの各集合をマージする
             Merge(array, left, mid, right, work);
@@ -49,6 +74,9 @@ namespace Sort_Merge1
         /// <param name="mid">マージする１つ目の集合の範囲の終了点</param>
         /// <param name="right">マージする２つ目の集合の範囲の終了点</param>
         /// <param name="work">マージを行うために必要な作業メモリ(マージ先集合として一時退避先として使用)</param>
+        /// <remarks>
+        /// 確かに無駄は多いかもしれないけど、わかりやすくていい。
+        /// </remarks>
         public static void Merge(int[] data, int left, int mid, int right, int[] work)
         {
             // 1つ目の集合の開始点をセット
@@ -79,13 +107,18 @@ namespace Sort_Merge1
                     k++;
                 }
             }
+
             // マージ済みでないデータが残っている集合をマージ先集合にマージ
+            // そのままデータを格納しているけど、ここに到達する地点で最初から昇順に並んだ状態で来るので、そのまま格納して問題ない。
+            // 例 : 0512って並びなら12はそのままの状態で問題ないでそ。それをそのまま入れるってわけ。
+            // 左側の残りを全て格納する。
             while (i <= mid)
             {
                 work[k] = data[i];
                 i++;
                 k++;
             }
+            // 右側の残りを全て格納する。
             while (j <= right)
             {
                 work[k] = data[j];
@@ -98,6 +131,12 @@ namespace Sort_Merge1
             {
                 data[left + x] = work[x];
             }
+
+            // C言語だとこうやらないといけないから大変だ。
+            ///* マージ先集合のサイズを計算 */
+            //size = right - left + 1;
+            ///* マージ先集合をdataにコピー */
+            //memcpy(&data[left], work, sizeof(int) * size);
         }
     }
 }
