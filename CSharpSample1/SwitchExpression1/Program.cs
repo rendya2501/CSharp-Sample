1198fd8e9ec17efb7eae71972cdafd72c246f331
rendya2501 <rendya2501@gmail.com>
2021-09-29 00:31:18 +0900

    9月28日分

--- CSharpSample1/SwitchExpression1/Program.cs
+++ CSharpSample1/SwitchExpression1/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
-namespace SandBox1
+namespace SwitchExpression1
 {
     class Program
     {
         /// <summary>
-        /// リフレクションを使ったプロパティへの代入はやっぱり遅いけど、1回20msなら別に気にする必要なくないか？
+        /// スイッチ式サンプル1
+        /// スイッチ式使ってActionを受け取って実行って事ができると、処理をifで切り替えずに済むのでは？と思ってやってみた。
+        /// 昔やったときは出来かったはずだが、できるようになってるっぽい。
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
             // 適当な分岐のつもり
             int flag = 1;
+
             // 1.Actionデリゲートとして受け取るタイプ
             Action result = flag switch
             {
@@ -22,13 +22,14 @@ namespace SandBox1
                 2 => Test2,
                 _ => throw new InvalidOperationException()
             };
-            result();
+            result.Invoke();
 
-            // 2.Actionを返却する関数として実装したタイプ
+            // 2.そもそも関数として切り離したタイプ
             Action(flag).Invoke();
 
             // 3.どれか1つでもキャストするとvarでもいけることがわかった。
             // でもって、こっちだと.Invoke()で起動できる。
+            // 勘違いだ。a()とするかa.Invoke()とするかの違いでしかなかった。
             var a = flag switch
             {
                 1 => (Action)Test1,
@@ -44,8 +45,15 @@ namespace SandBox1
                 2 => Test2,
                 _ => throw new InvalidOperationException()
             }).Invoke();
+            // 何とか1行に出来なくはないが・・・。ないだろうなぁ。
+            (flag switch { 1 => (Action)Test1, 2 => Test2, _ => throw new Exception() }).Invoke();
         }
 
+        /// <summary>
+        /// 2.そもそも関数として切り離したタイプ
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <returns></returns>
         static Action Action(int flag) => flag switch
         {
             1 => Test1,
@@ -53,8 +61,13 @@ namespace SandBox1
             _ => throw new InvalidOperationException()
         };
 
-
+        /// <summary>
+        /// テストメソッド1
+        /// </summary>
         static void Test1() => Console.WriteLine("test1");
+        /// <summary>
+        /// テストメソッド2
+        /// </summary>
         static void Test2() => Console.WriteLine("test2");
     }
 }
