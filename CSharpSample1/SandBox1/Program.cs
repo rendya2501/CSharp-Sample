c1c84988cc7c2589947e6d35ae281344880ba453
rendya2501 <rendya2501@gmail.com>
2021-11-15 00:06:54 +0900

    selectmany整理 decimal * 1検証

--- CSharpSample1/SandBox1/Program.cs
+++ CSharpSample1/SandBox1/Program.cs
@@ -5,24 +5,13 @@ using System.Reflection;
 
 namespace SandBox1
 {
+
     class Program
     {
-        /// <summary>
-        /// リフレクションを使ったプロパティへの代入はやっぱり遅いけど、1回20msなら別に気にする必要なくないか？
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int[][] arr = new int[2][];
-            //_ = arr[0].Length;
-
-            var aa = new int [100,512];
-            _ = aa.Length;
-            _ = aa.GetLength(0);
-
+            var aa = new Dictionary<int, string>();
+            var a2 = new List<string>();
         }
-
-        static void Test1() => Console.WriteLine("test1");
-        static void Test2() => Console.WriteLine("test2");
     }
 }
--- CSharpSample1/SelectMany/Program.cs
+++ CSharpSample1/SelectMany/Program.cs
@@ -18,6 +18,11 @@ namespace SelectMany
 
         static void Main(string[] args)
         {
+
+        }
+
+        static void sample1()
+        {
             //Parameter[] parameters = new Parameter[]
             //{
             //    new Parameter() { Name = "正一郎", Numbers = new int[] { 1, 2, 3 } },
@@ -27,7 +32,7 @@ namespace SelectMany
             //};
 
             // SelectManyのサンプルというより、タプルのサンプルになってしまった。
-            var parameters = new List<(string Name,int[] Numbers)> {
+            var parameters = new List<(string Name, int[] Numbers)> {
                 (Name: "正一郎", Numbers: new int[] {1, 2, 3 }),
                 (Name: "清次郎", Numbers: new int[] {1, 3, 5 }),
                 (Name: "誠三郎", Numbers: new int[] {2, 4, 6 }),
@@ -67,5 +72,63 @@ namespace SelectMany
             // 入力待ち用
             Console.ReadKey();
         }
+
+        static void sample2()
+        {
+            var authors = CreateAuthors();
+
+            var bookNames = authors.SelectMany(
+                author =>
+                {
+                    return author.Books.Select(book => book.Name);
+                },
+                (author, bookName) =>
+                {
+                    return $"{bookName}/{author.Name}";
+                }
+            );
+
+            Console.WriteLine(string.Join(", ", bookNames));
+        }
+
+        class Author
+        {
+            public string Name { get; set; }
+            public Book[] Books { get; set; }
+        }
+
+        class Book
+        {
+            public string Name { get; set; }
+        }
+
+        static Author[] CreateAuthors() =>
+            new[] {
+                new Author()
+                {
+                    Name = "芥川龍之介",
+                    Books = new[] {
+                        new Book(){Name = "羅生門"},
+                        new Book(){Name = "蜘蛛の糸"},
+                        new Book(){Name = "河童"},
+                    },
+                },
+                new Author()
+                {
+                    Name = "江戸川乱歩",
+                    Books = new[] {
+                        new Book(){Name = "人間椅子"},
+                        new Book(){Name = "怪人二十面相"},
+                    },
+                },
+                new Author()
+                {
+                    Name = "川端康成",
+                    Books = new[] {
+                        new Book(){Name = "雪国"},
+                        new Book(){Name = "伊豆の踊り子"},
+                    },
+                },
+            };
     }
 }
