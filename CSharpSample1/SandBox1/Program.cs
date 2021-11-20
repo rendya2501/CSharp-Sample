d16e775f988602b33060b17b25dffa53006a3621
rendya2501 <rendya2501@gmail.com>
2021-11-20 22:52:31 +0900

    シリアル化

--- CSharpSample1/SandBox1/Program.cs
+++ CSharpSample1/SandBox1/Program.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Xml.Serialization;
 
 namespace SandBox1
 {
+    //シリアライズ対象のクラス
+    public class SampleClass
+    {
+        public int Number;
+        public string Message;
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var aa = new Dictionary<int, string>();
-            var a2 = new List<string>();
+            SampleClass cls = new SampleClass
+            {
+                Message = "テスト",
+                Number = 123
+            };
+
+            var serializer = new XmlSerializer(typeof(SampleClass));
+            var sw = new StringWriter();
+            serializer.Serialize(sw, cls);
+            Console.WriteLine(sw.ToString());
+            sw.Close();
+
+            var ms = new MemoryStream();
+            serializer.Serialize(ms, cls);
+            ms.Position = 0;
+            var i = (SampleClass)serializer.Deserialize(ms);
+            Console.WriteLine(i.ToString());
+            ms.Close();
         }
     }
 }
