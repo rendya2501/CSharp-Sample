ac9990bbe9712cb10761619d337dbdcb50e61556
伊藤 駿 <rendya2501@gmail.com>
2022-06-02 23:30:47 +0900

    mvpサンプル追加

---
+++ CSharpSample1/MVPSample/Presenters/RectanglePresenter.cs
@@ -0,0 +1,25 @@
+﻿using MVPSample.Models;
+using MVPSample.Views;
+
+namespace MVPSample.Presenters
+{
+    public class RectanglePresenter
+    {
+        readonly IRectangle rectangleView;
+
+        public RectanglePresenter(IRectangle rectangleView)
+        {
+            this.rectangleView = rectangleView;
+        }
+
+        public void CalculateArea()
+        {
+            Rectangle rectangle = new()
+            {
+                Length = double.Parse(rectangleView.LengthText),
+                Breadth = double.Parse(rectangleView.BreadthText)
+            };
+            rectangleView.AreaText = rectangle.CalculateArea().ToString();
+        }
+    }
+}
