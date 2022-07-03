c857e2cd42160ba68c2622f6cf1680967f2316d6
伊藤 駿 <rendya2501@gmail.com>
2022-07-03 23:35:01 +0900

    CommunityToolkit.Mvvm

---
+++ Maui1/ViewModels/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+
+namespace Maui1.ViewModels
+{
+    public class ViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        public string FirstName;
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        public string LastName;
+
+        public string FullName => $"{FirstName},{LastName}";
+
+        public bool IsNotBusy => !IsBusy;
+
+        [ObservableProperty]
+        public bool IsBusy;
+        
+        [ICommand]
+        public void Tap()
+        {
+            IsBusy = true;
+            Console.WriteLine(FullName);
+            IsBusy = false;
+        }
+    }
+}
